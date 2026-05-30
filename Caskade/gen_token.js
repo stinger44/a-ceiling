ce09dd441acf57248cef289f93248f67537ab361
Eduard <34134311+stinger44@users.noreply.github.com>
2026-05-30 22:19:45 +0300

    cleanup: remove all infra and config files, keep only registration_bot

--- Caskade/gen_token.js
+++
@@ -1,4 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secret = '7uKRCoWcl8BPSYjteqpdJwHrE4FhmV6U902OI1NgbMafGL5nDTsxkQAy3ZzivX';
-const token = jwt.sign({ sub: '12345678-1234-1234-1234-123456789abc', type: 'api_token' }, secret);
-console.log(token);
